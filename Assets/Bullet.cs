c01a6d01f136b866a33e70dbb7ad2f54e3857a09
Jesse Palmer <unmodify@yahoo.com>
2018-03-07 12:19:12 -0500

    fixed Initial commit

---
+++ Assets/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    void OnCollisionEnter(Collision theCollision)
+    {
+        var hit = theCollision.gameObject;
+        var health = hit.GetComponent<Health>();
+        if (health != null)
+        {
+            health.TakeDamage(10);
+        }
+        Destroy(gameObject);
+    }
+
+// Use this for initialization
+void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Health.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public RectTransform healthBar;
+    public const int maxHealth = 100;
+    public int currentHealth = maxHealth;
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("Dead!");
+        }
+        healthBar.sizeDelta = new Vector2(    currentHealth,    healthBar.sizeDelta.y);
+    }
+}
