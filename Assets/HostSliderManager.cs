70528a822985835ef63cec6a856c30d716dad268
APriest <apriest@hydromassage.com>
2018-03-27 12:49:00 -0400

    can activate host and client with gaze on sliders

---
+++ Assets/HostSliderManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Networking;
+
+public class HostSliderManager : MonoBehaviour {
+
+	public NetworkManager manager;
+	Slider thisSlider;
+	float num = 0.01f;
+	bool activating = false;
+
+	bool hostFlag = false;
+
+
+	// Use this for initialization
+	void Start () {
+		manager = GameObject.Find("Network Manager").GetComponent<NetworkManager> ();
+		thisSlider = gameObject.GetComponent<Slider> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (activating) {
+			thisSlider.value += num;
+		} else {
+			thisSlider.value -= num;
+		}
+
+		thisSlider.value = Mathf.Max (0f, thisSlider.value);
+		thisSlider.value = Mathf.Min (1f, thisSlider.value);
+
+		if (thisSlider.value == 1 && !hostFlag) {
+			manager.StartHost ();
+			hostFlag = true;
+		}
+
+
+	}
+
+	public void activate(GameObject theObj, PointerEventData theData){
+		if (theObj.name == "Host Slider") {
+			activating = true;
+		}
+	}
+
+	public void deactivate(GameObject theObj, PointerEventData theData){
+		if (theObj.name == "Host Slider") {
+			activating = false;
+		}
+	}
+}
